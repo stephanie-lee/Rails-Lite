0fab8942eebb6e2f62d2dec2fdbb09cb321806fb
Steph <lee.en.wei@gmail.com>
2016-01-14 13:45:47 -0800

    finished methods for controller, router, and session

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -6,35 +6,58 @@ require_relative './session'
 class ControllerBase
   attr_reader :req, :res, :params
 
-  # Setup the controller
   def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @params = route_params.merge(req.params)
+    @already_built_response = false
   end
 
-  # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
-  # Set the response status code and header
   def redirect_to(url)
+    raise "content rendered twice" if already_built_response?
+
+    @res.status = 302
+    @res["Location"] = url
+    @already_built_response = true
+
+    session.store_session(@res)
   end
 
-  # Populate the response with content.
-  # Set the response's content type to the given type.
-  # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "double render error" if already_built_response?
+
+    @res.write(content)
+    @res['Content-Type'] = content_type
+
+    @already_built_response = true
+
+    session.store_session(@res)
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
+    path = File.dirname(__FILE__)
+    template_path_name = File.join(
+      path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+
+    template_data = File.read(template_path_name)
+
+    render_content(
+      ERB.new(template_data).result(binding),
+      "text/html"
+    )
   end
 
-  # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
-
